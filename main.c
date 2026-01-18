cc3a527e709fef5b452df673759f3d54bf3670cf
Luccas Torres <luccasfontes125@gmail.com>
2026-01-18 19:28:03 -0300

    chore

--- main.c
+++ main.c
@@ -27,50 +27,31 @@ int main()
     Graph G = initGraph(tam);
     int adj, node, source;
     scanf("%d", &source);
-    
-    // for (i; i <= tam; i++) {
-    //     scanf("%d", &qtde_nodes);
-    //     // char s[30];
-    //     // scanf("%s", s);
-    //     // printf("%s\n", s);
-    //     for (int j = 0; j < qtde_nodes; j++) {
-    //         scanf("%d", &node);
-    //         insertArc(G, i, node);
-    //     }
-    // }
 
     while ((scanf("%d", &adj)) == 1) {
         scanf("%d", &node);
         insertArc(G, adj, node);
     }
 
-    // insertArc(G, 1, 2);
-    // insertArc(G, 1, 5);
-
-    // insertArc(G, 2, 1);
-    // insertArc(G, 2, 5);
-    // insertArc(G, 2, 4);
-    // insertArc(G, 2, 3);
-
-    // insertArc(G, 3, 2);
-    // insertArc(G, 3, 4);
-
-    // insertArc(G, 4, 3);
-    // insertArc(G, 4, 2);
-    // insertArc(G, 4, 5);
-
-    // insertArc(G, 5, 2);
-    // insertArc(G, 5, 1);
-    // insertArc(G, 5, 4);
-
     imprimir(G);
-    BFS(G, source);
+    Noh *OrdTop = NULL;
+    DFS_OrdTop(G, &OrdTop);
+    Noh *tmp = OrdTop;
+    if (OrdTop == NULL) puts("oiii");
+    while (tmp != NULL) {
+        printf("%d ", tmp->value);
+        tmp = tmp->prox;
+    }
 
     for (int i = 0; i < G->V; i++) {
-        printf("Pai do %d: %d ", i, G->pai[i]);
-        printf("Distancia do %i ao %d: %d ", i, source, G->dist[i]);
+        // printf("Pai do %d: %d ", i, G->pai[i]);
+        // printf("Distancia do %i ao %d: %d ", i, source, G->dist[i]);
+        printf("\nVértice %d: \nTempo de descoberta %d\nTempo de finalização %d", i, G->tempo[i].descoberta, G->tempo[i].fim);
         puts("");
     }
 
+    // int vert = 3;
+    // printf("Caminho de %d até %d\n", source, vert);
+    // printPath(G, source, vert);
     return 0;
 }
